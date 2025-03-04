c6c85b291099e592a3b46a026fe11e0f7eed2409
NeevBhandari13 <86910225+NeevBhandari13@users.noreply.github.com>
2025-03-05 07:53:59 +1100

    finished TestValidatePlayerInfo

--- backend/go.mod
+++ backend/go.mod
@@ -13,12 +13,16 @@ require (
 	cloud.google.com/go/compute/metadata v0.6.0 // indirect
 	cloud.google.com/go/firestore v1.18.0 // indirect
 	cloud.google.com/go/longrunning v0.6.2 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/go-logr/logr v1.4.2 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/google/s2a-go v0.1.8 // indirect
 	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
 	github.com/googleapis/gax-go/v2 v2.14.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.5.2 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/google.golang.org/grpc/otelgrpc v0.54.0 // indirect
 	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.54.0 // indirect
 	go.opentelemetry.io/otel v1.29.0 // indirect
@@ -37,4 +41,5 @@ require (
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20241209162323-e6fa225c2576 // indirect
 	google.golang.org/grpc v1.68.0 // indirect
 	google.golang.org/protobuf v1.35.2 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- backend/internal/game/game_test.go
+++ backend/internal/game/game_test.go
@@ -3,15 +3,24 @@ package game
 import (
 	"testing"
 	"werewolf/protos"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 )
 
+type FirestoreMock struct {
+	mock.Mock
+}
+
 func TestValidatePlayerInfo(t *testing.T) {
+
 	// define a struct to hold all the tests
 	tests := []struct {
 		name      string             // label for test
 		player    *protos.PlayerInfo // player input
 		expectErr bool               // whether or not the test should return an error
 	}{
+		// arrange
 		{"Good player", &protos.PlayerInfo{PlayerName: "Bob", PlayerId: "1"}, false},
 		{"Nil Player", nil, true},
 		{"Missing Player ID", &protos.PlayerInfo{PlayerName: "Bob"}, true},
@@ -19,14 +28,21 @@ func TestValidatePlayerInfo(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// act
 			err := ValidatePlayerInfo(tt.player)
-			if tt.expectErr && err == nil {
-				t.Errorf("expected error but got nil")
+			// assert
+			if tt.expectErr {
+				// if i use requires instead of assert, testing will stop at first failure
+				assert.NotNil(t, err)
 			}
-			if !tt.expectErr && err != nil {
-				t.Errorf("expected no error, got %v", err)
+			if !tt.expectErr {
+				assert.Nil(t, err)
 			}
 		})
 	}
+
+}
+
+func TestGenerateGameId(t *testing.T) {
 
 }
